df14916fb00fdbfdaca1f9a110dda290c1124524
nadia027 <nadia0207@hotmail.com>
2026-04-29 16:18:28 +0200

    Se mejoro la funcion de eliminar columnas y se agrego la funcion cardinalidad

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,14 +1,40 @@
+import numpy as np
+import pandas as pd
 
-def elimina_columnas_id(df):
-    columnas = ['Visit ID','Patient ID','Hospital ID']
-    df_final = df.drop(columns = columnas)
-    return df_final
-    
-def elimina_Total_Wait_Time(df):
-    df_final = df.drop(columns = 'Total Wait Time (min)')
-    return df_final
+def card_tipo(df,umbral_categoria = 10, umbral_continua = 30):
+    # Primera parte: Preparo el dataset con cardinalidades, % variación cardinalidad, y tipos
+    df_temp = pd.DataFrame([df.nunique(), df.nunique()/len(df) * 100, df.dtypes]) # Cardinaliad y porcentaje de variación de cardinalidad
+    df_temp = df_temp.T # Como nos da los valores de las columnas en columnas, y quiero que estas sean filas, la traspongo
+    df_temp = df_temp.rename(columns = {0: "Card", 1: "%_Card", 2: "Tipo"}) # Cambio el nombre de la transposición anterior para que tengan más sentido, y uso asignación en vez de inplace = True (esto es arbitrario para el tamaño de este dataset)
 
-def elimina_Visit_Date(df):
-    df_final = df.drop(columns = 'Visit Date')   
-    return df_final
+    # Corrección para cuando solo tengo un valor
+    df_temp.loc[df_temp.Card == 1, "%_Card"] = 0.00
+
+    # Creo la columna de sugerenica de tipo de variable, empiezo considerando todas categóricas pero podría haber empezado por cualquiera, siempre que adapte los filtros siguientes de forma correspondiente
+    df_temp["tipo_sugerido"] = "Categorica"
+    df_temp.loc[df_temp["Card"] == 2, "tipo_sugerido"] = "Binaria"
+    df_temp.loc[df_temp["Card"] >= umbral_categoria, "tipo_sugerido"] = "Numerica discreta"
+    df_temp.loc[df_temp["%_Card"] >= umbral_continua, "tipo_sugerido"] = "Numerica continua"
+    # Ojo los filtros aplicados cumplen con el enunciado pero no siguen su orden y planteamiento
+
+    return df_temp
+
+# ========================================================================================
+'''
+'Visit ID',
+'Patient ID',
+'Hospital ID',
+Total Wait Time (min),
+'Visit Date',
+
+'Hospital Name' 
+'Region'
+'''
+
+def elimina_columna(df, *columnas):
+    return df.drop(columns = list(columnas)) 
+#errors = 'ignore' para ignorar el error si la columna no existe
+
+
+
 
